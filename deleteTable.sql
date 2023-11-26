525c65af3a5f4b960605f1ba5db2a62ee43f7a45
mindfoo <amppascoal@gmail.com>
2023-11-26 00:19:07 +0000

    deletes and drops

--- deleteTable.sql
+++ deleteTable.sql
@@ -1,6 +1,13 @@
 /*
  * Apagar todas as tabelas criadas.
  * */
-drop table r;
-drop table s;
-drop table t;
+
+drop table from bicicleta;
+drop table from classica;
+drop table from clientereserva;
+drop table from dispositivo;
+drop table from eletrica;
+drop table from loja;
+drop table from pessoa;
+drop table from reserva;
+drop table from telefoneloja;
--- removeTable.sql
+++ removeTable.sql
@@ -1,4 +1,12 @@
 /*
  * Delete de dados das tabelas criadas.
  * */
-delete from t;
+delete from bicicleta;
+delete from classica;
+delete from clientereserva;
+delete from dispositivo;
+delete from eletrica;
+delete from loja;
+delete from pessoa;
+delete from reserva;
+delete from telefoneloja;
