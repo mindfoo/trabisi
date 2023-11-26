2151d76c59304a06bd5222a7c7609242fcbcc462
Bernardo Pontes <bernardopontes97@gmail.com>
2023-11-26 16:28:07 +0000

    edit queries

--- ex3/queries.sql
+++ ex3/queries.sql
@@ -1,23 +1,26 @@
-/*
- * Queries Trabalho 2.
- * */
+/* Conceba, na linguagem PostgreSQL, as interrogações que produzam os resultados a seguir indicados, utilizando apenas uma instrução PostgreSQL. 
+Guarde todas num script autónomo de nome “queries.sql”. 
+Para cada instrução deve ser também apresentada, em comentário, a descrição do raciocínio seguido. */
 
-delete from t where c=3;
 
-insert into s values (1,2);
+-- (a) Implemente em PostgreSQL as interrogações pedidas na alínea 2 (Pode escolher uma das soluções que apresentou acima, desde que nesta fase consiga fazer uso de todos os operadores).
 
-update t set c = 5 where c = 4;
+-- (b) Obtenha os nomes de todos os clientes que fizeram pelo menos uma reserva numa loja localizada em Lisboa.
 
-/* 
-(a) Implement in PostgreSQL the questions requested in 2. (You can choose one of the above solutions if you can use all the operators at this stage).
-(b) Retrieve the names of all customers who made a reservation in a store located in Lisbon.
-(c) Find the serial numbers of devices with a battery percentage greater than 50%, and list them in ascending order of their battery percentage.
-(d) Retrieve the brand and the model (marca and modelo, respectively) of the bicycle with the highest autonomy (autonomia) among the available bikes.
-(e) Retrieve the total number of reservations for each store along with their code (codigo) and the total number of reservations.
-(f) Retrieve all the stores (codigo and email) that have made more than 5 bookings to date and list them in descending order of the number of bookings (Note: Make use of the date and time operators/functions).    
-(g) Find the names of the customers who made reservations with bicycles having an estado (status) of ’under maintenance’ (“em manutencao”) last year (Note: Make use of the date and time operators/functions).
-(h) Retrieve the information (nome, morada and telefone) of people who managed a store and have made reservations.
-(i) Retrieve the names of customers who made reservations at a store managed by a person named “John” (assuming “John” is the manager of a store).
-(j) Create a view LISTJOAOFILIPE that includes information about the customers who have made bookings at a shop run by “Jo ̃ao Filipe”. (Note: Jo ̃ao Filipe is also a customer).
-(k) Create a view BICYCLESINNUMBERS that includes information about the bicycles and their number. The information available should be the type of bike (electric and classic), its status (“under maintenance” and “other”4) and the total number.
-*/
+-- (c) Encontre os núumeros de série dos dispositivos com uma percentagem de bateria superior a 50%, e liste-os por ordem crescente da sua percentagem de bateria.
+
+-- (d) Apresente a marca e o modelo da bicicleta com maior autonomia dentro das bicicletas disponíveis.
+
+-- (e) Obter o número total de reservas para cada loja, bem como o seu código e o número total de reservas.
+
+-- (f)Liste todas as lojas (codigo e email) que tenham efectuado mais de 5 reservas, até á presente data, e enumere-as por ordem decrescente do número de reservas (Nota: Faça uso dos operadores/funções de data e tempo).
+
+-- (g) Apresente os nomes dos clientes que efectuaram reservas de bicicletas cujo estado tem como valor “em manutencao” no ano passado (Nota: Faça uso dos operadores/funções de data e tempo).
+
+-- (h) Listar as informações (nome, morada, telefone) das pessoas que geriram uma loja e efectuaram reservas.
+
+-- (i) Obter o(s) nome(s) do(s) cliente(s) que realizaram reservas numa loja gerida por uma pessoa chamada “João”.
+
+-- (j) Crie uma vista LISTAJOAOFILIPE que inclui informação sobre os clientes que efectuaram reservas num loja gerida pelo “João Filipe”. (Nota: O João Filipe é, ele também cliente).
+
+-- (k) Crie uma vista BICICLETASEMNUMEROS que inclui informação sobre as bicicletas e o seu número. A informação disponível deverá ser, o tipo de bicicleta (elétrica e clássica), o seu estado (“em manutenção” e “outras”2) e o número total.
