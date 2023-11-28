123de0515c1e42e358ffdee332263e08ee9e684b
mindfoo <amppascoal@gmail.com>
2023-11-28 21:41:52 +0000

    removing begin and commit from queries

--- ex3/queries.sql
+++ ex3/queries.sql
@@ -15,52 +15,46 @@
 */
 
 -- 2(a) Pretende-se obter informação (nome, morada e telefone) dos clientes e dos gestores
-begin;
 select nome, morada, telefone
 from pessoa
 where atrdisc = 'G' or atrdisc = 'C';
-commit;
+
 
 
 -- 2(b) Liste, agora, informação (nome, morada e telefone) sobre os clientes que também são gestores.
-begin;
 select distinct nome, morada, telefone
 from pessoa, clientereserva
 where pessoa.atrdisc = 'G'
 and pessoa.id = clientereserva.cliente;
-commit;
+
 
 
 -- 2(c) Pretende-se saber que pessoas (nome, morada e telefone) não estão associadas a nenhuma reserva.
-begin;
 select distinct pessoa.nome, pessoa.morada, pessoa.telefone
 from pessoa
 left join clientereserva on pessoa.id = clientereserva.cliente
 left join reserva on clientereserva.cliente = reserva.noreserva
 where reserva.noreserva is null;
-commit;
+
 
 
 -- 2(d)  Apresente a lista de bicicletas (marca, modelo e estado) que não estão associadas a nenhuma reserva e não são eléctricas.
-begin;
 select marca, modelo, estado
 from bicicleta
 where bicicleta.atrdisc = 'C'
 and bicicleta.estado <> 'ocupado';
-commit;
+
 
 
 -- 2 (e) O conjunto de dispositivos (noserie, latitude e longitude) de bicicletas cujo estado encontra-se em “em manutencao”.
-begin;
 select distinct noserie, latitude, longitude
 from dispositivo  
 join bicicleta on bicicleta.id = dispositivo.noserie
 where bicicleta.estado = 'em manutenção';
-commit;     
+     
 
 
 -- 2 (f) O nome dos clientes que realizaram reservas com bicicletas electricas. Apresente informacao sobre os clientes e o numero de reservas.
-begin;
 select pessoa.nome, count(clientereserva.reserva) as numero_reservas
 from pessoa
 join clientereserva on pessoa.id = clientereserva.cliente
@@ -68,40 +62,36 @@ join reserva on clientereserva.reserva = reserva.noreserva and clientereserva.lo
 join bicicleta on reserva.bicicleta = bicicleta.id
 where bicicleta.atrdisc = 'E'
 group by pessoa.nome;
-commit;
+
 
 
 -- 2(g) Pretende-se obter a lista de clientes que efectuaram reservas com um valor total superior a e 100 (e.g.).
-begin;
 select distinct pessoa.nome
 from pessoa
 join clientereserva on pessoa.id = clientereserva.cliente
 join reserva on clientereserva.reserva = reserva.noreserva and clientereserva.loja = reserva.loja 
 where reserva.valor > 100;
-commit;
+
 
 
 -- 2(h) Liste informacoes (email, endereco e localidade) sobre lojas e respectivos numeros de telefone associados, incluindo lojas que podem nao ter um numero de telefone associado.
-begin;
 select loja.email, loja.endereco, loja.localidade, telefoneloja.numero
 from loja
 left join telefoneloja on loja.codigo = telefoneloja.loja;
-commit;
+
 
 
 -- 2(i) Para o cliente de nome “José Manuel”, pretende-se a lista de reservas (noreserva e loja) que efectuou, nomeadamente a sua data e as horas de inıcio e de fim, e o preço ofinal desta.
-begin;
 select distinct reserva.noreserva, reserva.loja, reserva.dtinicio, reserva.dtfim, reserva.valor
 from pessoa 
 join clientereserva on pessoa.id = clientereserva.cliente
 join reserva on clientereserva.reserva = reserva.noreserva and clientereserva.loja = reserva.loja
 join loja on reserva.loja = loja.codigo
 where pessoa.nome = 'José Manuel';
-commit;
+
 
 
 -- 2(j) Apresente a lista do(s) cliente(s) (nome, morada, telefone e nacionalidade), com mais reservas no ano de 2023.
-begin;
 select pessoa.nome, pessoa.morada, pessoa.telefone, pessoa.nacionalidade, count(*) as nmr_reservas
 from pessoa
 join clientereserva on pessoa.id = clientereserva.cliente
@@ -118,15 +108,14 @@ having count(*) = (
     group by clientereserva.cliente
   ) as subquery
 );
-commit;
+
 
 
 -- 2 (k) Apresente o número de clientes de nacionalidade portuguesa e outros. O resultado deve mostrar os atributos nacionalidade e o número de clientes.
-begin;
 select pessoa.nacionalidade , count(*) as total
 from pessoa
 group by nacionalidade;
-commit;  
+  
      
 
 /*
@@ -139,7 +128,6 @@ commit;
 * agrupa os resultados pelo nome
 *
 */
-begin;
 select distinct nome
 from pessoa, clientereserva, reserva, loja
 where pessoa.id = clientereserva.cliente
@@ -147,7 +135,7 @@ and clientereserva.loja = reserva.loja
 and loja.localidade = 'Lisboa'
 group by pessoa.nome
 having count(*) >=  1;
-commit;
+
     
     
 /*
@@ -158,12 +146,11 @@ commit;
 * ordena as percentagens da bateria em ordem crescente
 *
 */
-begin;
 select noserie, bateria
 from dispositivo
 where bateria > 50
 order by bateria;
-commit;
+
 
 
 /* 
@@ -175,14 +162,13 @@ commit;
 * limita a visualisação dos resultados apenas ao primeiro tupulo
 *
 */
-begin;
 select marca, modelo
 from bicicleta
 join eletrica on bicicleta.id = eletrica.bicicleta
 where eletrica.autonomia =(
     select max(autonomia)
     from eletrica);
-commit;
+
 
 
 /*
@@ -190,12 +176,11 @@ commit;
 * (e) Obter o número total de reservas para cada loja, bem como o seu código e o número total de reservas.
 *
 */
-begin;
 select codigo, count(*) as treservas
 from loja
 join reserva on loja.codigo = reserva.loja
 group by loja.codigo;
-commit;
+
 
 
 /*
@@ -210,7 +195,6 @@ commit;
 * inclui apenas lojas com mais de 5 reservas
 *
 */
-begin;
 select codigo, email, count(*) as nreservas
 from loja
 join reserva on loja.codigo = reserva.loja
@@ -218,7 +202,7 @@ where reserva.dtinicio <= current_date
 group by loja.codigo, loja.email
 having count(*) > 5
 order by nreservas desc; 
-commit;
+
 
 
 /*
@@ -232,7 +216,6 @@ commit;
 * onde se apenas consideram clientes e bicicletas em manutenção relativas ao ano passado
 *
 */
-begin;
 select distinct nome
 from pessoa, clientereserva, reserva, bicicleta
 where pessoa.id = clientereserva.cliente
@@ -240,7 +223,7 @@ and clientereserva.reserva = reserva.noreserva
 and reserva.bicicleta = bicicleta.id
 and bicicleta.estado = 'em manutenção'
 and extract(year from reserva.dtinicio) = extract(year from current_date) - 1;
-commit;
+
 
 
 /* 
@@ -252,11 +235,10 @@ commit;
 * seleccionar apenas os gestores
 *
 */
-begin;
 select distinct nome, morada, telefone 
 from pessoa, clientereserva, loja
 where pessoa.id = clientereserva.cliente and loja.gestor = pessoa.id;
-commit;
+
 
 
 /*
@@ -271,7 +253,6 @@ commit;
 * mostrar apenas o nome do cliente
 *
 */
-begin;
 select distinct pessoa_cliente.nome
 from pessoa as pessoa_cliente
 join clientereserva on pessoa_cliente.id = clientereserva.cliente
@@ -279,7 +260,7 @@ join reserva on clientereserva.reserva = reserva.noreserva and clientereserva.lo
 join loja on reserva.loja = loja.codigo
 join pessoa as pessoa_gestor on loja.gestor = pessoa_gestor.id
 where pessoa_gestor.nome like '%João%';
-commit;
+
 
 
 /*
@@ -290,7 +271,6 @@ commit;
 *  funcionamento igual á alinea (i), sendo que o select será de todos os atributos da pessoa e o gestor é "João Filipe"
 *
 */
-begin;
 create view LISTAJOAOFILIPE as
 select distinct pessoa_cliente.nome, pessoa_cliente.morada, pessoa_cliente.email, pessoa_cliente.telefone, pessoa_cliente.noident, pessoa_cliente.nacionalidade, pessoa_cliente.atrdisc
 from pessoa as pessoa_cliente
@@ -299,7 +279,7 @@ join reserva on clientereserva.reserva = reserva.noreserva and clientereserva.lo
 join loja on reserva.loja = loja.codigo
 join pessoa as pessoa_gestor on loja.gestor = pessoa_gestor.id
 where pessoa_gestor.nome = 'João Filipe';
-commit;
+
 
 select * from LISTAJOAOFILIPE; -- visualisar a vista criada
 
@@ -314,7 +294,6 @@ select * from LISTAJOAOFILIPE; -- visualisar a vista criada
 *	agrupar por tipo e status
 *
 */
-begin;
 create view BICICLETASEMNUMEROS as
 select
   case
@@ -328,6 +307,6 @@ select
   count(*) as total
 from bicicleta
 group by tipo, status;
-commit;
+
 
 select * from BICICLETASEMNUMEROS;  -- visualisar a vista criada
